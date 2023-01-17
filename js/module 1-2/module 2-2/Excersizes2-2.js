78556a3477fdead0a9fcb682d761fd0a6c2718a2
NovicovMykhailo <mikenovicov@gmail.com>
2023-01-17 17:55:24 +0200

    dodelat'

--- js/module 1-2/module 2-2/Excersizes2-2.js
+++ js/module 1-2/module 2-2/Excersizes2-2.js
@@ -5,3 +5,15 @@
 
 
 const langs = ['python', 'javascript', 'c++', 'haskel', 'php', 'ruby'];
+
+// console.table(langs[1][0]>'a')
+
+for( let i=0; i < langs.length; i+=1){
+
+    if(langs[i][0] > langs[i + 1]){
+        langs.splice(i, 1)[0];
+    }
+}
+
+
+console.log(langs)
