c6793748353336484017d7dee7767c22a42d136a
NovicovMykhailo <mikenovicov@gmail.com>
2023-01-17 19:27:53 +0200

    ex6

---
+++ js/module 1-2/module 2-2/Ex-7.js
@@ -0,0 +1,11 @@
+// ## Example 7 - Сортировка массива с циклом
+
+// Напиши скрипт сортировки массива строк в алфавитном порядке по первой букве
+// элемента.
+
+
+const langs = ['python', 'javascript', 'c++', 'haskel', 'php', 'ruby'];
+
+langs.sort();
+
+console.log(langs)
