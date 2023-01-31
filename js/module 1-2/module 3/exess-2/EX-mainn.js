46d0f96799893cb113b9b5cdac646af3067aa674
NovicovMykhailo <mikenovicov@gmail.com>
2023-01-31 20:59:46 +0200

    Update EX-mainn.js

--- js/module 1-2/module 3/exess-2/EX-mainn.js
+++ js/module 1-2/module 3/exess-2/EX-mainn.js
@@ -5,11 +5,11 @@
 // Напиши функцию `transformUsername(user)` так, чтобы она возвращала новый обьект
 // со свойством `fullName`, вместо `firstName` и `lastName`.
 
-transformUsername(user){
-  return {
+// transformUsername(user){
+//   return {
 
-  }
-}
+//   }
+// }
 
 // ```js
 // // Решение
@@ -40,3 +40,87 @@ transformUsername(user){
 //   }),
 // );
 // ```
+
+
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+
+// const getUsersWithEyeColor = (usersArray, color) => {
+//   return usersArray.filter(user => user.eyeColor === color )
+  
+// };
+
+const getUsersWithFriend = (usersArray, friendName) => {
+  let usersFriends = usersArray.filter(a => a.friends)
+  return usersFriends}
+
+
+console.log(getUsersWithFriend(users, "Goldie Gentry"))
+
+ //функция возвращает массив объектов пользователей с именами Moore Hensley, Sharlene Bush и Carey Barr
+//  "green", //функция возвращает массив объектов пользователей с именами Ross Vazquez и Elma Head
+//  "brown", //функция возвращает массив объектов пользователей с именами Blackburn Dotson и Sheree Anthony
